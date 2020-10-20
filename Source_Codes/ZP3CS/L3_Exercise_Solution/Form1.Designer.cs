809b7724f5732e46f7278a308c4f2f5adcb17133
Petr Janis <petr.janis@colorcare.cz>
2020-10-20 17:49:00 +0200

    lecture 3 exercise solution

--- Source_Codes/ZP3CS/L3_Exercise_Solution/Form1.Designer.cs
+++ Source_Codes/ZP3CS/L3_Exercise_Solution/Form1.Designer.cs
@@ -30,52 +30,185 @@
         {
             this.colorDropDown = new System.Windows.Forms.ComboBox();
             this.showColorBtn = new System.Windows.Forms.Button();
-            this.colorPanel = new System.Windows.Forms.Panel();
+            this.dropDownListColorPanel = new System.Windows.Forms.Panel();
+            this.textBoxRed = new System.Windows.Forms.TextBox();
+            this.RGBColorPanel = new System.Windows.Forms.Panel();
+            this.textBoxBlue = new System.Windows.Forms.TextBox();
+            this.textBoxGreen = new System.Windows.Forms.TextBox();
+            this.textBoxColorName = new System.Windows.Forms.TextBox();
+            this.saveColorButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // colorDropDown
             // 
             this.colorDropDown.FormattingEnabled = true;
-            this.colorDropDown.Location = new System.Drawing.Point(25, 15);
+            this.colorDropDown.Location = new System.Drawing.Point(33, 18);
+            this.colorDropDown.Margin = new System.Windows.Forms.Padding(4);
             this.colorDropDown.Name = "colorDropDown";
-            this.colorDropDown.Size = new System.Drawing.Size(213, 21);
+            this.colorDropDown.Size = new System.Drawing.Size(283, 24);
             this.colorDropDown.TabIndex = 0;
             // 
             // showColorBtn
             // 
-            this.showColorBtn.Location = new System.Drawing.Point(25, 53);
+            this.showColorBtn.Location = new System.Drawing.Point(33, 65);
+            this.showColorBtn.Margin = new System.Windows.Forms.Padding(4);
             this.showColorBtn.Name = "showColorBtn";
-            this.showColorBtn.Size = new System.Drawing.Size(212, 23);
+            this.showColorBtn.Size = new System.Drawing.Size(283, 28);
             this.showColorBtn.TabIndex = 1;
             this.showColorBtn.Text = "Show selected color";
             this.showColorBtn.UseVisualStyleBackColor = true;
             this.showColorBtn.Click += new System.EventHandler(this.ShowColorBtn_Click);
             // 
-            // colorPanel
+            // dropDownListColorPanel
             // 
-            this.colorPanel.Location = new System.Drawing.Point(25, 120);
-            this.colorPanel.Name = "colorPanel";
-            this.colorPanel.Size = new System.Drawing.Size(212, 181);
-            this.colorPanel.TabIndex = 2;
+            this.dropDownListColorPanel.Location = new System.Drawing.Point(33, 148);
+            this.dropDownListColorPanel.Margin = new System.Windows.Forms.Padding(4);
+            this.dropDownListColorPanel.Name = "dropDownListColorPanel";
+            this.dropDownListColorPanel.Size = new System.Drawing.Size(283, 223);
+            this.dropDownListColorPanel.TabIndex = 2;
+            // 
+            // textBoxRed
+            // 
+            this.textBoxRed.Location = new System.Drawing.Point(461, 20);
+            this.textBoxRed.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxRed.Name = "textBoxRed";
+            this.textBoxRed.Size = new System.Drawing.Size(132, 22);
+            this.textBoxRed.TabIndex = 3;
+            this.textBoxRed.Text = "0";
+            this.textBoxRed.TextChanged += new System.EventHandler(this.TextBoxRed_TextChanged);
+            // 
+            // RGBColorPanel
+            // 
+            this.RGBColorPanel.Location = new System.Drawing.Point(545, 65);
+            this.RGBColorPanel.Margin = new System.Windows.Forms.Padding(4);
+            this.RGBColorPanel.Name = "RGBColorPanel";
+            this.RGBColorPanel.Size = new System.Drawing.Size(283, 223);
+            this.RGBColorPanel.TabIndex = 4;
+            // 
+            // textBoxBlue
+            // 
+            this.textBoxBlue.Location = new System.Drawing.Point(803, 20);
+            this.textBoxBlue.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxBlue.Name = "textBoxBlue";
+            this.textBoxBlue.Size = new System.Drawing.Size(132, 22);
+            this.textBoxBlue.TabIndex = 5;
+            this.textBoxBlue.Text = "0";
+            this.textBoxBlue.TextChanged += new System.EventHandler(this.TextBoxBlue_TextChanged);
+            // 
+            // textBoxGreen
+            // 
+            this.textBoxGreen.Location = new System.Drawing.Point(633, 20);
+            this.textBoxGreen.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxGreen.Name = "textBoxGreen";
+            this.textBoxGreen.Size = new System.Drawing.Size(132, 22);
+            this.textBoxGreen.TabIndex = 6;
+            this.textBoxGreen.Text = "0";
+            this.textBoxGreen.TextChanged += new System.EventHandler(this.TextBoxGreen_TextChanged);
+            // 
+            // textBoxColorName
+            // 
+            this.textBoxColorName.Location = new System.Drawing.Point(545, 311);
+            this.textBoxColorName.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxColorName.Name = "textBoxColorName";
+            this.textBoxColorName.Size = new System.Drawing.Size(132, 22);
+            this.textBoxColorName.TabIndex = 7;
+            // 
+            // saveColorButton
+            // 
+            this.saveColorButton.Location = new System.Drawing.Point(728, 308);
+            this.saveColorButton.Margin = new System.Windows.Forms.Padding(4);
+            this.saveColorButton.Name = "saveColorButton";
+            this.saveColorButton.Size = new System.Drawing.Size(100, 28);
+            this.saveColorButton.TabIndex = 8;
+            this.saveColorButton.Text = "Save Color";
+            this.saveColorButton.UseVisualStyleBackColor = true;
+            this.saveColorButton.Click += new System.EventHandler(this.SaveColorButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(433, 23);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 16);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "R";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(775, 23);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 16);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "B";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(605, 22);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(18, 16);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "G";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(457, 315);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 16);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Color Name";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.colorPanel);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.saveColorButton);
+            this.Controls.Add(this.textBoxColorName);
+            this.Controls.Add(this.textBoxGreen);
+            this.Controls.Add(this.textBoxBlue);
+            this.Controls.Add(this.RGBColorPanel);
+            this.Controls.Add(this.textBoxRed);
+            this.Controls.Add(this.dropDownListColorPanel);
             this.Controls.Add(this.showColorBtn);
             this.Controls.Add(this.colorDropDown);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
 
         private System.Windows.Forms.ComboBox colorDropDown;
         private System.Windows.Forms.Button showColorBtn;
-        private System.Windows.Forms.Panel colorPanel;
+        private System.Windows.Forms.Panel dropDownListColorPanel;
+        private System.Windows.Forms.TextBox textBoxRed;
+        private System.Windows.Forms.Panel RGBColorPanel;
+        private System.Windows.Forms.TextBox textBoxBlue;
+        private System.Windows.Forms.TextBox textBoxGreen;
+        private System.Windows.Forms.TextBox textBoxColorName;
+        private System.Windows.Forms.Button saveColorButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- Source_Codes/ZP3CS/L3_Exercise_Solution/Form1.cs
+++ Source_Codes/ZP3CS/L3_Exercise_Solution/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Forms;
 
@@ -6,41 +7,126 @@ namespace L3_Exercise_Solution
 {
     public partial class Form1 : Form
     {
-        private Color[] _colors;
+        private readonly Dictionary<string, Color> _colors;
 
         public Form1()
         {
+            _colors = new Dictionary<string, Color>();
             InitializeComponent();
-
             LoadColors();
             AddColorsToDropDown();
+
+            // Initialize controls before first change in text boxes happens.
+            ColorChanged();
         }
 
+        // Adds predefined colors to drop down.
         private void LoadColors()
         {
-            _colors = new Color[4];
-            _colors[0] = Color.Black;
-            _colors[1] = Color.Blue;
-            _colors[2] = Color.Green;
-
-            // Creates struct color from 3 integers (R,G,B).
-            var color = Color.FromArgb(255, 170, 170);
-            _colors[3] = color;
+            _colors.Add("Black", Color.Black);
+            _colors.Add("Blue", Color.Blue);
+            _colors.Add("Green", Color.Green);
         }
 
         private void AddColorsToDropDown()
         {
             foreach (var color in _colors)
             {
-                // Items is property that represents items that will be shown to user in drop down.
-                colorDropDown.Items.Add(color);
+                colorDropDown.Items.Add(color.Key);
             }
         }
 
         private void ShowColorBtn_Click(object sender, EventArgs e)
         {
-            var selectedColor = (Color)colorDropDown.SelectedItem;
-            colorPanel.BackColor = selectedColor;
+            var selectedColorText = colorDropDown.SelectedItem.ToString();
+
+            // Gets value from dictionary by it's key. In our case it's name of the color.
+            dropDownListColorPanel.BackColor = _colors[selectedColorText];
+        }
+
+        private void TextBoxRed_TextChanged(object sender, EventArgs e)
+        {
+            ValidateTextBoxValue(textBoxRed);
+
+            ColorChanged();
+        }
+
+        private void TextBoxBlue_TextChanged(object sender, EventArgs e)
+        {
+            ValidateTextBoxValue(textBoxBlue);
+
+            ColorChanged();
+        }
+
+        private void TextBoxGreen_TextChanged(object sender, EventArgs e)
+        {
+            ValidateTextBoxValue(textBoxGreen);
+
+            ColorChanged();
+        }
+
+        // Easier ways to do this. But not cover on lecture yet.
+        private void ValidateTextBoxValue(TextBox textBox)
+        {
+            try
+            {
+                var number = int.Parse(textBox.Text);
+                if (number < 0 || number > 255)
+                {
+                    SetTextBoxValueToZero(textBox);
+                }
+            }
+            catch (Exception)
+            {
+                SetTextBoxValueToZero(textBox);
+            }
+        }
+
+        private void SetTextBoxValueToZero(TextBox textBox)
+        {
+            textBox.Text = "0";
+        }
+
+
+        private void ColorChanged()
+        {
+            // Creates new color from entered values inside text boxes. 
+            var color = Color.FromArgb(
+                int.Parse(textBoxRed.Text),
+                int.Parse(textBoxGreen.Text),
+                int.Parse(textBoxBlue.Text));
+
+            // Shows color in panel.
+            RGBColorPanel.BackColor = color;
+        }
+
+        private void SaveColorButton_Click(object sender, EventArgs e)
+        {
+            // Checks if name of color is filled and is unique. Otherwise shows message box.
+            if (string.IsNullOrEmpty(textBoxColorName.Text) == false &&
+                _colors.ContainsKey(textBoxColorName.Text) == false)
+            {
+                var newColor = Color.FromArgb(
+                    int.Parse(textBoxRed.Text),
+                    int.Parse(textBoxGreen.Text),
+                    int.Parse(textBoxBlue.Text));
+
+                // Adds color with it's name into collection _colors.
+                _colors.Add(textBoxColorName.Text, newColor);
+
+                // Add color name into drop down list.
+                colorDropDown.Items.Add(textBoxColorName.Text);
+
+                // Resets user controls.
+                textBoxColorName.Text = string.Empty;
+                SetTextBoxValueToZero(textBoxRed);
+                SetTextBoxValueToZero(textBoxGreen);
+                SetTextBoxValueToZero(textBoxBlue);
+            }
+            else
+            {
+                MessageBox.Show(@"Name of color has to be defined and has to be unique.");
+            }
         }
     }
 }
